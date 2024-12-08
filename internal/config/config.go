3eaab3ce8a10bc65c74d9692966a3c8b20c09c37
Ayaulym <zhumasheva.aya@gmail.com>
2024-12-09 00:16:04 +0500

    add some pacjages

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"time"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
@@ -18,17 +19,23 @@ type HTTPServer struct {
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s" `
 }
 
-func MustLoad() {
+func MustLoad() *Config {
 	configPath := os.Getenv("CONFIG_PATH")
 
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH is not set")
 	}
 
-	if _, err:=os.Stat(configPath); os.IsNotExist(err){
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		log.Fatalf("config file does not exist: %s", configPath)
 	}
 
-	var cgf 
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config %s", err)
+	}
+
+	return &cfg
 
 }
